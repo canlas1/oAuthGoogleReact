1138589cdf82f2a38243024037dcaffa4f1f2f6e
canlas1 <dc@uscr.com>
2017-07-10 16:48:01 -0400

    sample for team react.js oAuth with Auth0, we still need to write it to mongoDB

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'auththis.auth0.com',
+  clientId: 'e3LP0qsR05mty9QgF2MZsmY67CvMjnUf',
+  callbackUrl: 'http://localhost:3000/callback',
+  apiUrl: 'http://auththis.com'
+}
